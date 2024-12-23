3d24ce0057b1cef04360ed8b0091eb69639c5470
hubele <scotthubele@live.com>
2024-12-23 04:33:41 +0100

    New setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ Created on Sun Dec 22 21:10:20 2024
 
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1' 
+VERSION = '0.0.2' 
 DESCRIPTION = 'Simulations and analysis of images from K39 quantum gas microscope'
 
 # Setting up
@@ -16,7 +16,7 @@ setup(
         name="sisi", 
         version=VERSION,
         author="Scott Hubele",
-        url='git@github.com:las-m/SingleSiteImaging.git',
+        url='git@github.com/las-m/SingleSiteImaging.git',
         author_email="<scott.hubele@uni-hamburg.de>",
         description=DESCRIPTION,
         packages=find_packages(),
