1bcd4bac5dddc7550bb9b90bba526db73b130ff6
hubele <scotthubele@live.com>
2024-12-23 04:07:21 +0100

    Update 1-PseudoCode-ImagingSim.py

--- SampleScripts/1-PseudoCode-ImagingSim.py
+++ SampleScripts/1-PseudoCode-ImagingSim.py
@@ -5,4 +5,27 @@ Created on Sun Dec 22 20:58:01 2024
 @author: scott
 """
 
-from sisi import coordinate_transform_helpers
+import sisi.coordinate_transform_helpers as sisisim
+import sisi.psf_functions
+
+# Atom plane parameters
+fov = 30e-6
+
+# Imaging parameters
+magnification = 50
+psf_func = sisi.psf_functions.gaussian_2d
+psf_params = [0, 0, 313e-9, 313e-9, 0]
+
+atom_plane_params = {
+    'fov' : fov
+    }
+
+imaging_plane_params = {
+    'magnification' : magnification,
+    'psf_func'      : psf_func,
+    'psf_params'    : psf_params
+    }
+
+imaging_system = sisisim.Experiment()
+imaging_system.setAtomPlaneParams(**atom_plane_params)
+imaging_system.addImagingPlane(**imaging_plane_params)
